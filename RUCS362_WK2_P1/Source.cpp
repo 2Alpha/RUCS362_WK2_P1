bae888ce9288d5edba7758f5f41e972289d5a23c
2alpha <andrae3000@gmail.com>
2015-09-08 16:11:35 -0400

    create search mechnism

--- RUCS362_WK2_P1/Source.cpp
+++ RUCS362_WK2_P1/Source.cpp
@@ -20,9 +20,13 @@ classLevel convertCredits(int inputCredit);
 string convertClassLevel2String(classLevel enumeratedTypeValue);
 
 void ProgramDescription();
-void readDataFile1(string inputFileName, string studentID[], char studentClassLevel[], string classLevelAsString[], int credits[]);
+void readDataFile1(string inputFileName, string studentID[], char studentClassLevel[], classLevel classLevelAsOrdinalValue[], string classLevelAsString[], int credits[], int& studentIndex);
 
 void StoreUndergradData (string[], int[]);
+
+void selectSortByStudentID(int numOfItems, string alphaNumericList[], classLevel enumeratedTypeValue[]);
+void createSortedTextFile(int numOfItems, string alphaNumericList[], classLevel enumeratedTypeValue[]);
+void displayStudentSearch(int numOfItems, string alphaNumericList[]);
 
 
 const int MAX_ENTRIES = 10; // Max number of student profiles that can be stored
@@ -30,23 +34,28 @@ const int FRESHMAN_BOUNDARY = 32;
 const int SOPHOMORE_BOUNDARY = 63;
 const int JUNIOR_BOUNDARY = 95;
 const int SENIOR_BOUNDARY = 95;
+const int MAX_COLUMNS = 5; 
 
 
-const string FILE_1_NAME = "CREDITS.txt"; // File name for input file
+const string INPUT_FILE_1_NAME = "CREDITS.txt"; // File name for input file
+const string OUTPUT_FILE_1_NAME = "SORTED.txt";
 
 int main()
 {
 	string StudentIDList[MAX_ENTRIES];
 	string clasLevelAsString[MAX_ENTRIES];
 	char ClassLevelList[MAX_ENTRIES];
+	classLevel classLevelAsOVList[MAX_ENTRIES];
 	int CreditsList[MAX_ENTRIES];
+	int numOfEntries;
 
 
 	
 	ProgramDescription();
-	readDataFile1(FILE_1_NAME, StudentIDList, ClassLevelList, clasLevelAsString, CreditsList);
-
-
+	readDataFile1(INPUT_FILE_1_NAME, StudentIDList, ClassLevelList, classLevelAsOVList, clasLevelAsString, CreditsList, numOfEntries);
+	selectSortByStudentID(numOfEntries, StudentIDList, classLevelAsOVList);
+	createSortedTextFile(numOfEntries, StudentIDList, classLevelAsOVList);
+	displayStudentSearch(numOfEntries, StudentIDList);
 
 
 	system("PAUSE");
@@ -75,7 +84,7 @@ void ProgramDescription()
 //  INPUT:        Parameters:  
 //  OUTPUT: 	  Return value: fileOpenSuccess
 //*************************************************************************
-void readDataFile1(string inputFileName, string studentID[], char studentClassLevel[], string classLevelAsString[], int credits[])
+void readDataFile1(string inputFileName, string studentID[], char studentClassLevel[], classLevel classLevelAsOrdinalValue[], string classLevelAsString[], int credits[], int& studentIndex)
 {
 	ifstream inputFile;                      // input file stream variable
 	bool fileOpenSuccess;
@@ -84,7 +93,7 @@ void readDataFile1(string inputFileName, string studentID[], char studentClassLe
 	char tempStudentClassLevel;
 	int tempCredits;
 
-	int studentIndex = 0;
+	studentIndex = 0;
 	inputFile.open(inputFileName.c_str());       // open the file
 
 	if (!inputFile)
@@ -119,6 +128,7 @@ void readDataFile1(string inputFileName, string studentID[], char studentClassLe
 			studentID[studentIndex] = tempStudentID;
 			studentClassLevel[studentIndex] = tempStudentClassLevel; 
 			credits[studentIndex] = tempCredits;
+			classLevelAsOrdinalValue[studentIndex] = convertCredits(credits[studentIndex]);
 			classLevelAsString[studentIndex] = convertClassLevel2String(convertCredits(credits[studentIndex]));
 			studentIndex++;
 		}
@@ -136,9 +146,10 @@ void readDataFile1(string inputFileName, string studentID[], char studentClassLe
 	
 	for (int index = 0; index < studentIndex; index++)
 	{
-		cout << "student id           " << studentID[index] << endl;
-		cout << "class Level          " << studentClassLevel[index] << endl;
-		cout << "student Credits      " << credits[index] << endl;
+		cout << "student id                  " << studentID[index] << endl;
+		cout << "class Level                 " << studentClassLevel[index] << endl;
+		cout << "student Credits             " << credits[index] << endl;
+		cout << "class level ordinal value   " << classLevelAsOrdinalValue[index] << endl;
 		cout << classLevelAsString[index] << endl; 
 		cout << endl;
 	}
@@ -147,10 +158,6 @@ void readDataFile1(string inputFileName, string studentID[], char studentClassLe
 
 		inputFile.close();
 
-
-		//cout << "converted credits = " << convertCredits(96) << endl;
-		
-		//cout << convertClassLevel2String(convertCredits(5)) << endl;
 
 }
 
@@ -204,6 +211,86 @@ string convertClassLevel2String(classLevel enumeratedTypeValue)
 	}
 
 	return classLevelAsString; 
+
+
+}
+
+// Implementation of a selection sort
+void selectSortByStudentID(int numOfItems, string alphaNumericList[], classLevel enumeratedTypeValue[])
+{
+	int currentTop,            // Current top of unsorted list
+		tryIndex,            // Position to compare value to
+		minPosition;       // Position of smallest value
+		
+	string tempString;              // Temp value for swapping
+	classLevel tempClassLevel;
+
+	// for each item in the list (top to bottom)
+	for (currentTop = 0; currentTop < numOfItems - 1; currentTop++)
+	{
+
+		minPosition = currentTop;      // start with current top as smallest value
+
+		// find smallest value from currentTop down
+		for (tryIndex = currentTop + 1; tryIndex < numOfItems; tryIndex++)
+			if (alphaNumericList[tryIndex] > alphaNumericList[minPosition]) // Flip Sign to invert sort order
+				minPosition = tryIndex;
+
+		// if smallest not at currentTop, swap with currentTop
+		if (minPosition != currentTop)
+		{
+			tempString = alphaNumericList[currentTop];
+			tempClassLevel = enumeratedTypeValue[currentTop];
+
+			alphaNumericList[currentTop] = alphaNumericList[minPosition];
+			enumeratedTypeValue[currentTop] = enumeratedTypeValue[minPosition];
+
+			alphaNumericList[minPosition] = tempString;
+			enumeratedTypeValue[minPosition] = tempClassLevel;
+
+
+		}   // end swap
+
+	}  // end for
+
+	return;
+} // end selectionSort
+
+void createSortedTextFile(int numOfItems, string alphaNumericList[], classLevel enumeratedTypeValue[])
+{
+	ofstream sortedList; 
+	sortedList.open(OUTPUT_FILE_1_NAME.c_str());     // open file for writing
+	char skip = ' ';
+
+	for (int index = 0; index < numOfItems; index++)
+	{
+		cout << "intList = " << alphaNumericList[index] << "    " << enumeratedTypeValue[index] << endl;
+		sortedList << alphaNumericList[index] << skip << enumeratedTypeValue[index] << endl;
+
+	}
+
+	sortedList.close();     // close output file
+
+
+}
+
+void displayStudentSearch (int numOfItems, string alphaNumericList[])
+{
+	int numOfColums = MAX_COLUMNS;
+	
+	cout << "Student ID numbers on file are :" << endl;
+	int index = 0; 
+
+	while (index < numOfItems)
+	{
+		for (int columnPosition = 0; columnPosition < MAX_COLUMNS; columnPosition++)
+
+		{
+			cout << alphaNumericList[index] << "    ";
+			index++;
+		}
+		cout << endl; 
+	}
 
 
 }
